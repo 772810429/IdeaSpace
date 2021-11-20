3556bcfc6c1af02942a075a3a072a0afcc16e517
LIWEI <772810429@qq.com>
2021-11-21 01:02:32 +0800

    增加一行github测试2。

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("ZHEGzheg 这个是程序");
         System.out.println("进行github上传测试");
+        System.out.println("进行github上传测试1");
     }
 }
